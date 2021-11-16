cd82b4c8dae87de7f6046f3503a6a1293261b550
Cory Accardo <accardo@usc.edu>
2021-11-15 16:49:10 -0800

    removed compression

--- next.config.js
+++ next.config.js
@@ -1,28 +1,17 @@
 
-const withOptimizedImages = require("next-optimized-images");
 const withFonts = require("next-fonts");
   const withImages = require('next-images')
 
 
-module.exports = {
-    ...withImages(),
-    future: {
-       webpack5: true,
-    },
- 
- }
 
-module.exports = withFonts(
-  withOptimizedImages({
+module.exports = withFonts({
+    ...withImages(),
     images: {
         disableStaticImages: true
-      }
-    /* config for next-optimized-images */
-    // your config for other plugins or the general next.js here...
-    // images: {
-    //   path: "/src/assets/image",
-    // },
-  })
+      },
+    webpack5: false,
+}
+
 );
 
 
