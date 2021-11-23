db2decc580527504be3d572c1cf415a867d3152e
bilal-firekamp <73871362+bilal-firekamp@users.noreply.github.com>
2021-11-24 04:05:02 +0700

    try

--- src/sections/hotcoolcafe/SectionDetail.js
+++ src/sections/hotcoolcafe/SectionDetail.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import checkoutImg from "../../assets/image/hotcool/png/checkout-icon-1.png";
 
 const SectionDetail = ({ className, ...rest }) => {
     return (
@@ -26,16 +25,11 @@ const SectionDetail = ({ className, ...rest }) => {
                                     we are now excited to have created our very own Brand of Coffee.
                                     Check out our Crenshaw Coffee Co. blends & the Best Vegan Bar food in L.A.
                                 </p>
-                            </div>
-                            <div className="d-flex align-items-center justify-content-center mr-xl-15">
-                                <div className="mr-3">
-                                    <img src={checkoutImg} alt="" style={{ width: '14px' }} />
-                                </div>
-                                <div className="mt-0">
-                                    <p className="font-size-6 mb-0 font-weight-bold">
-                                        <a style={{ color: '#000' }} href="https://hotandcoolcafe.com/" target="_blank">Hot + Cool Cafe</a>
-                                    </p>
-                                </div>
+                                <div className="mt-10 text-center">
+                    <a href="#" target="_blank" className="btn btn-black btn-1 rounded-5">
+                        Visit website
+                    </a>
+                </div>
                             </div>
                         </div>
                     </div>
