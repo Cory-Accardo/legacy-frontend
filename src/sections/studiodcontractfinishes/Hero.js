094be6a7ee92c80b75de26fa407bd18ed6f94f44
Cory <accardo@usc.edu>
2021-11-27 14:57:57 -0800

    added studio

--- src/sections/studiodcontractfinishes/Hero.js
+++ src/sections/studiodcontractfinishes/Hero.js
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 import arrow from "../../assets/image/arrow.png";
 import imgM from "../../assets/image/studiodcontractfinishes/png/hero-image.png";
-import purchase from '../../utils/purchase';
+import BuyButton from '../../sections/common/BuyButton'
 
 const Hero = ({ className, ...rest }) => {
   return (
@@ -39,9 +39,9 @@ const Hero = ({ className, ...rest }) => {
 
                   <div className="col-lg-5">
 
-                    <button onClick={() => purchase('prod_Kdy1tQ82a0V2fR')} className="btn btn-dark-green btn-3 rounded-5 text-white" target="_blank" style={{ background: '#a52062', borderColor: '#a52062' }}>
+                    <BuyButton productId='prod_Kdy1tQ82a0V2fR' className="btn btn-dark-green btn-3 rounded-5 text-white" target="_blank" style={{ background: '#a52062', borderColor: '#a52062' }}>
                       Book Your Consultation
-                    </button>
+                    </BuyButton>
 
                   </div>
                   <div className="col-lg-7">
